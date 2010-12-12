ca5bdc4f334d1a2485caebe8a1e5056ca645a6ed
Stefan Becker <stefan.becker@nokia.com>
2010-12-12 20:37:55 +0200

    add source module for www.tvnyt.fi
    Suggested in SF feature request #3135167

---
+++ grab/fi/fi/source/tvnyt.pm
@@ -0,0 +1,16 @@
+# -*- mode: perl; coding: utf-8 -*- ###########################################
+#
+# tv_grab_fi: source specific grabber code for http://www.tvnyt.fi
+#
+###############################################################################
+#
+# Setup
+#
+# INSERT FROM HERE ############################################################
+package fi::source::tvnyt;
+
+# Description
+sub description { 'tvnyt.fi' }
+
+# That's all folks
+1;
