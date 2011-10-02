0e43eb95a2bde2a63d5711453e283eb8bd50c1d0
Stefan Becker <chemobejk@gmail.com>
2011-10-02 16:22:17 +0300

    sources: add offset parameter to grab() class method
    Offset has the same meaning as the --offset parameter on the command
    line, i.e. 0 means today, 1 means tomorrow.

--- grab/fi/fi/source/telkku.pm
+++ grab/fi/fi/source/telkku.pm
@@ -76,7 +76,7 @@ sub channels {
 
 # Grab one day
 sub grab {
-  my($self, $id, $yesterday, $today, $tomorrow) = @_;
+  my($self, $id, $yesterday, $today, $tomorrow, $offset) = @_;
 
   # Get channel number from XMLTV id
   return unless my($channel) = ($id =~ /^(\d+)\.telkku\.com$/);
