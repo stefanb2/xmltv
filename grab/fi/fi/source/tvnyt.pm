36af39c9bef0faa853217bc2b7ee38a47d79db41
Stefan Becker <chemobejk@gmail.com>
2012-01-16 21:39:30 +0200

    tvnyt: re-implement channels() method

--- grab/fi/fi/source/tvnyt.pm
+++ grab/fi/fi/source/tvnyt.pm
@@ -27,7 +27,87 @@ sub description { 'tv.nyt.fi' }
 
 # Grab channel list
 sub channels {
-  return;
+  my %channels;
+  my @groups = ( "free_air_fi" );
+  my $added;
+
+  # Next group
+  while (defined(my $group = shift(@groups))) {
+
+    # Fetch & parse HTML
+    my $root = fetchTree("http://tv.nyt.fi/home/tvnyt_grid/?group=$group");
+    if ($root) {
+
+      #
+      # Group list can be found in dropdown
+      #
+      #  <select id="group_select" ...>
+      #    <option value="tvnyt*today*free_air_fi" selected>...</option>
+      #    <option value="tvnyt*today*sanoma_fi">...</option>
+      #    ...
+      #  </select>
+      #
+      unless ($added) {
+	if (my $container = $root->look_down("id" => "group_select")) {
+	  if (my @options = $container->find("option")) {
+	    debug(2, "Source tv.nyt.fi found " . scalar(@options) . " groups");
+            foreach my $option (@options) {
+	      unless ($option->attr("selected")) {
+		my $value = $option->attr("value");
+
+		if (defined($value) &&
+		    (my($tag) = ($value =~ /^tvnyt\*today\*(\w+)$/))) {
+		  debug(3, "group '$tag'");
+		  push(@groups, $tag);
+		}
+	      }
+	    }
+	  }
+	}
+	$added++;
+      }
+
+      #
+      # Channel list can be found in table headers
+      #
+      #  <table class="grid_table" cellspacing="0px">
+      #    <thead>
+      #      <tr>
+      #        <th class="yle_tv1">...</th>
+      #        <th class="yle_tv2">...</th>
+      #        ...
+      #      </tr>
+      #    </thead>
+      #    ...
+      #  </table>
+      #
+      if (my $container = $root->look_down("class" => "grid_table")) {
+	my $head = $container->find("thead");
+	if ($head && (my @headers = $head->find("th"))) {
+	  debug(2, "Source tv.nyt.fi found " . scalar(@headers) . " channels in group '$group'");
+	  foreach my $header (@headers) {
+	      if (my $image = $header->find("img")) {
+		my $name = $image->attr("alt");
+		my $channel_id = $header->attr("class");
+
+		if (defined($channel_id) && length($channel_id) &&
+		    defined($name)       && length($name)) {
+		  debug(3, "channel '$name' ($channel_id)");
+		  $channels{"${channel_id}.${group}.tv.nyt.fi"} = "fi $name";
+		}
+	      }
+	    }
+	}
+      }
+
+      # Done with the HTML tree
+      $root->delete();
+    }
+
+  }
+
+  debug(2, "Source tv.nyt.fi parsed " . scalar(keys %channels) . " channels");
+  return(\%channels);
 }
 
 # Grab one day
