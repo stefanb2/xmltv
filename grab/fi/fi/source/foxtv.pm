9a8be5dd1de3f89e9a58ec190ef6538472d33393
Stefan Becker <chemobejk@gmail.com>
2013-10-13 02:53:32 +0300

    foxtv: add module for new data source
    FOX program guide at http://www.foxtv.fi/ohjelmat/
    
    Based on the code provided by Lasse Miettinen in Feature Request #102.

---
+++ grab/fi/fi/source/foxtv.pm
@@ -0,0 +1,139 @@
+# -*- mode: perl; coding: utf-8 -*- ###########################################
+#
+# tv_grab_fi: source specific grabber code for http://www.foxtv.fi
+#
+###############################################################################
+#
+# Setup
+#
+# VERSION: $Id: telkku.pm,v 1.999 yyyy/mm/dd hh:mm:ss xxx Exp $
+#
+# INSERT FROM HERE ############################################################
+package fi::source::foxtv;
+use strict;
+use warnings;
+
+BEGIN {
+  our $ENABLED = 1;
+}
+
+# Import from internal modules
+fi::common->import();
+fi::programmeStartOnly->import();
+
+# Description
+sub description { 'foxtv.fi' }
+
+# Grab channel list - only one channel available, no need to fetch anything...
+sub channels { { 'foxtv.fi' => 'fi FOX' } }
+
+# Grab one day
+sub grab {
+  my($self, $id, $yesterday, $today, $tomorrow, $offset) = @_;
+
+  # Get channel number from XMLTV id
+  return unless ($id eq "foxtv.fi");
+
+  #
+  # Only the weekly page contains all the information we need. Each of the 7
+  # days in a week will return the same weekly information, although the URL
+  # will be different. This will break XMLTV caching.
+  #
+  # The weekly page starts on Monday. We simply calculate what the Monday of
+  # the week is that contains the day we want to grab.
+  #
+  my $wday;
+  my $url;
+  {
+    # Epoch of today at 12:00
+    my $epoch = timeToEpoch($today, 12, 0);
+
+    # localtime weekday: 0: Sunday, 1: Monday,  ..., 6: Saturday
+    # foxtv.fi weekday:  0: Monday, 1: Tuesday, ..., 6: Sunday
+    $wday = ((localtime($epoch))[6] + 6) % 7;
+
+    # Epoch of today (if it is Monday) or the previous Monday, at
+    # 11:00 if Monday (standard time) -> today (daylight saving time)
+    # 12:00 if no daylight saving change during the week
+    # 13:00 if Monday (daylight saving time) -> today (standard time)
+    my($mday, $mon, $year) = (localtime($epoch - $wday * 86400))[3..5];
+    $mon  += 1;
+    $year += 1900;
+
+    # URL for weekly page
+    $url = "http://www.foxtv.fi/ohjelmat/weekly/$mday.$mon.$year";
+  }
+
+  # Fetch & parse HTML
+  my $root = fetchTree($url);
+  if ($root) {
+    my $opaque = startProgrammeList();
+
+    if (my $container = $root->look_down("_tag"  => "table",
+					 "class" => "bloque-slider")) {
+      if (my @table_entries = $container->look_down("_tag"  => "td",
+						    "class" => undef)) {
+	if (my @programmes = $table_entries[$wday]->look_down("class" => qr/^itemListings/)) {
+
+	  foreach my $programme (@programmes) {
+	    my $title = $programme->look_down("_tag" => "h4");
+	    my $time  = $programme->look_down("_tag"  => "span",
+					      "class" => undef);
+	    my $desc  = $programme->look_down("class" => "ShowDescription colLeft");
+
+	    if ($title && $time && $desc) {
+	      $time  = $time->as_text();
+
+	      if (my($hour, $minute) = $time =~ /^(\d{2}):(\d{2})/) {
+		my($season, $episode_number) = $programme->look_down("_tag" => "h5");
+		my $episode_name             = $programme->look_down("_tag" => "p");
+
+		$desc  = $desc->as_text();
+		$title = $title->as_text();
+
+		# Too short description -> assume empty. Kind of hackish :-(
+		undef $desc if (length($desc) < 3);
+
+		# Season, episode number & episode name
+		if ($season) {
+		  $season = $season->as_text();
+		  $season =~ s/[^\d]//g;
+		  undef $season if (length($season) == 0);
+		}
+		if ($episode_number) {
+		  $episode_number = $episode_number->as_text();
+		  $episode_number =~ s/[^\d]//g;
+		  undef $episode_number if (length($episode_number) == 0);
+		}
+		if ($episode_name) {
+		  $episode_name = $episode_name->as_text();
+		  $episode_name =~ s/^\s+//;
+		  $episode_name =~ s/\s+$//;
+		  undef $episode_name if (length($episode_name) == 0);
+		}
+
+		debug(3, "List entry fox ($hour:$minute) $title");
+		debug(4, $episode_name) if defined $episode_name;
+		debug(4, $desc)         if defined $desc;
+		debug(4, sprintf("s%02de%02d", $season, $episode_number))
+		    if (defined($season) && defined($episode_number));
+
+		appendProgramme($opaque, $hour, $minute, $title, undef, $desc);
+	      }
+	    }
+	  }
+	}
+      }
+    }
+
+    # Done with the HTML tree
+    $root->delete();
+
+    return(convertProgrammeList($opaque, $id, "fi", $yesterday, $today, $tomorrow));
+  }
+
+  return;
+}
+
+# That's all folks
+1;
