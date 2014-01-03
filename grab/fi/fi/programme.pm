024914e65d9bcc56c8891d5e5134ad6b9a2dfb5e
Stefan Becker <chemobejk@gmail.com>
2014-01-03 21:13:25 +0200

    sync with version in sourceforge CVS

--- grab/fi/fi/programme.pm
+++ grab/fi/fi/programme.pm
@@ -6,7 +6,7 @@
 #
 # Setup
 #
-# VERSION: $Id: programme.pm,v 2.02 2013/11/01 22:55:13 stefanb2 Exp $
+# VERSION: $Id: programme.pm,v 2.03 2014/01/03 19:11:36 stefanb2 Exp $
 #
 # INSERT FROM HERE ############################################################
 package fi::programme;
