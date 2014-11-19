2ae88a382549ee3860df3ca3cdce8b8983bbb477
Stefan Becker <chemobejk@gmail.com>
2014-11-19 20:41:40 +0200

    yle: update channel() method
    Yle URLs have changed again: the language code is no longer included in
    the URL. Instead both languages use a different host name now. Use a
    mapping table between language code and host name to avoid configuration
    changes.
    
    Updated test.conf with 2 new channels added to yle.fi.

--- grab/fi/fi/source/yle.pm
+++ grab/fi/fi/source/yle.pm
@@ -25,15 +25,20 @@ fi::programmeStartOnly->import();
 # Description
 sub description { 'yle.fi' }
 
+my %languages = (
+    "fi" => "ohjelmaopas",
+    "sv" => "programguide",
+);
+
 # Grab channel list
 sub channels {
   my %channels;
 
   # yle.fi offers program guides in multiple languages
-  foreach my $code ("fi", "sv") {
+  foreach my $code (sort keys %languages) {
 
     # Fetch & parse HTML
-    my $root = fetchTree("http://ohjelmaopas.yle.fi/lang/$code?path=tv/opas");
+    my $root = fetchTree("http://$languages{$code}.yle.fi/tv/opas");
     if ($root) {
 
       #
