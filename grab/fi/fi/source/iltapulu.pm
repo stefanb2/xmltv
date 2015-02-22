07541bd7852f3133e790d6f9f58efa5acc8dd921
Stefan Becker <chemobejk@gmail.com>
2015-02-22 20:40:05 +0200

    iltapulu: fix day grabbing

--- grab/fi/fi/source/iltapulu.pm
+++ grab/fi/fi/source/iltapulu.pm
@@ -39,7 +39,7 @@ sub channels {
     #
     # Channel list can be found in table rows
     #
-    #  <table class="channel-row"">
+    #  <table class="channel-row">
     #   <tbody>
     #    <tr>
     #     <td class="channel-name">...</td>
@@ -49,19 +49,21 @@ sub channels {
     #   </tbody>
     #   ...
     #  </table>
+    #  ...
     #
     if (my @tables = $root->look_down("class" => "channel-row")) {
       foreach my $table (@tables) {
-	my @cells = $table->look_down("class" => "channel-name");
-	foreach my $cell (@cells) {
-	  if (my $image = $cell->find("img")) {
-	    my $name = $image->attr("alt");
-	    $name =~ s/\s+tv-ohjelmat$//;
+	if (my @cells = $table->look_down("class" => "channel-name")) {
+	  foreach my $cell (@cells) {
+	    if (my $image = $cell->find("img")) {
+	      my $name = $image->attr("alt");
+	      $name =~ s/\s+tv-ohjelmat$//;
 
-	    if (defined($name) && length($name)) {
-	      my $channel_id = (scalar(keys %channels) + 1) . ".iltapulu.fi";
-	      debug(3, "channel '$name' ($channel_id)");
-	      $channels{$channel_id} = "fi $name";
+	      if (defined($name) && length($name)) {
+		my $channel_id = (scalar(keys %channels) + 1) . ".iltapulu.fi";
+		debug(3, "channel '$name' ($channel_id)");
+		$channels{$channel_id} = "fi $name";
+	      }
 	    }
 	  }
 	}
@@ -76,84 +78,103 @@ sub channels {
   return(\%channels);
 }
 
-# Parse time and convert to seconds since midnight
-sub _toEpoch($$$$) {
-  my($today, $tomorrow, $time, $switch) = @_;
-  my($hour, $minute) = ($time =~ /^(\d{2})(\d{2})$/);
-  return(timeToEpoch($switch ? $tomorrow : $today, $hour, $minute));
-}
-
 # Grab one day
 sub grab {
   my($self, $id, $yesterday, $today, $tomorrow, $offset) = @_;
 
   # Get channel number from XMLTV id
-  return unless my($channel, $group) = ($id =~ /^([-\w]+)\.([-\w]+)\.tv\.hs\.fi$/);
-
-  # Replace Dash with Underscore for URL
-  $channel =~ s/-/_/g;
-  $group   =~ s/-/_/g;
+  return unless my($channel) = ($id =~ /^([-\w]+)\.iltapulu\.fi$/);
 
   # Fetch & parse HTML
-  my $root = fetchTree("http://tv.hs.fi/home/grid?group=${group}&date=" . $today->ymdd());
+  my $root = fetchTree("http://www.iltapulu.fi/?all=1&date=" . $today->ymdd());
   if ($root) {
+    my $count = 0;
     my @objects;
 
     #
-    # Programme data is contained inside a table cells with class="<channel>"
+    # Programme data is contained inside a div class="<full-row>"
     #
-    #  <td class="yle_tv1">
-    #   <table class="be_list_table">
-    #    <tr class="s1210 e1230"> (start/end time, "+" for tomorrow)
-    #     <td class="be_time">12:10</td>
-    #     <td class="be_entry">
-    #      <span class="thb1916041"></span>
-    #      <span class="flw6390"></span>
-    #      <a href="/programs/show/1916041" class="program_link colorbox tip">
-    #       Hercules... (title)
-    #      </a>
-    #      <span class="tooltip">
-    #       <span class="wl_actions">...</span>
-    #       <span class="wl_synopsis">
-    #        Dokumenttielokuva bulgarialaisen perheen... (long description)
-    #       </span>
-    #      </span>
-    #      <span class="syn">
-    #       Dokumenttielokuva bulgarialaisen... (short description)
-    #      </span>
-    #     </td>
+    #  <table class="channel-row">
+    #   <tbody>
+    #    <tr>
+    #     <td class="channel-name">...</td>
+    #     <td class="channel-name">...</td>
+    #     ...
     #    </tr>
-    #   ...
-    #   </table>
-    #  </td>
+    #    <tr class="full-row...">
+    #     <td>
+    #      <div class="schedule">
+    #       <div class="full-row" data-starttime="1424643300" data-endtime="1424656800">
+    #        <table>
+    #         <tr>
+    #          <td class="time">00.15</td>
+    #          <td class="title">
+    #           <a class="program-open..." ... title="... description ...">
+    #            Uutisikkuna
+    #           </a>
+    #          </td>
+    #         </tr>
+    #        </table>
+    #       </div>
+    #      </div>
+    #      ...
+    #     </td>
+    #     ...
+    #    </tr>
+    #    ...
+    #   </tbody>
+    #  </table>
+    #  ...
     #
-    if (my @cells = $root->look_down("class" => $channel,
-				     "_tag"  => "td")) {
-      foreach my $cell (@cells) {
-	foreach my $row ($cell->find("tr")) {
-	  my $start_stop = $row->attr("class");
-	  my $entry      = $row->look_down("class" => "be_entry");
-          if (defined($start_stop) && $entry &&
-	      (my($start, $stomorrow, $end, $etomorrow) =
-	       ($start_stop =~ /^s(\d{4})(\+?)\s+e(\d{4})(\+?)$/))) {
-	    my $title = $entry->look_down("class" => qr/program_link/);
-            my $desc  = $entry->look_down("class" => "wl_synopsis");
-	    if ($title) {
-	      $title = $title->as_text();
-              if (length($title)) {
-		$start = _toEpoch($today, $tomorrow, $start, $stomorrow);
-		$end   = _toEpoch($today, $tomorrow, $end,   $etomorrow);
-		$desc  = $desc->as_text() if $desc;
+    if (my @tables = $root->look_down("class" => "channel-row")) {
 
-		debug(3, "List entry ${channel}.${group} ($start -> $end) $title");
-		debug(4, $desc) if $desc;
+     TABLES:
+      foreach my $table (@tables) {
+	if (my @cells = $table->look_down("class" => "channel-name")) {
 
-		# Create program object
-		my $object = fi::programme->new($id, "fi", $title, $start, $end);
-		$object->description($desc);
-		push(@objects, $object);
+	  # Channel in this table?
+	  my $index = $channel - $count - 1;
+	  $count   += @cells;
+	  if ($channel <= $count) {
+
+	    # Extract from each row the div's from the same index
+	    my @divs;
+	    if (my @rows = $table->look_down("_tag"  => "tr",
+					     "class" => qr/full-row/)) {
+	      foreach my $row (@rows) {
+		my $children = $row->content_array_ref;
+		if ($children) {
+		  my $td = $children->[$index];
+		  push(@divs, $td->look_down("class" => "full-row"))
+		    if defined($td);
+		}
 	      }
 	    }
+
+	    for my $div (@divs) {
+	      my $start = $div->attr("data-starttime");
+	      my $end   = $div->attr("data-endtime");
+	      my $link  = $div->look_down("class" => qr/program-open/);
+
+	      if ($start && $end && $link) {
+		my $title = $link->as_text();
+
+		if (length($title)) {
+		  my $desc = $link->attr("title");
+
+		  debug(3, "List entry ${id} ($start -> $end) $title");
+		  debug(4, $desc) if $desc;
+
+		  # Create program object
+		  my $object = fi::programme->new($id, "fi", $title, $start, $end);
+		  $object->description($desc);
+		  push(@objects, $object);
+		}
+	      }
+	    }
+
+	    # skip the rest of the data
+	    last TABLES;
 	  }
 	}
       }
